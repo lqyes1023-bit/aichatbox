cd94acf3106ec3237fe56ec718f82d25bb4db3c7
lqyes1023-bit <lqyes1023@gmail.com>
2026-05-15 22:59:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ return """ <h1>Claude Scanner Running 💚</h1> <p>Cloud Run 已成功启动</p>
 
 if **name** == "**main**":
 
-```
+
 port = int(os.environ.get("PORT", 8080))
 
 app.run(
     host="0.0.0.0",
     port=port
 )
-```
+
